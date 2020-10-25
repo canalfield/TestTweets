098d5d096ea870d485190f8f53a751aeeb962184
hamudust <hamudust@yahoo.co.jp>
2020-10-25 20:26:04 +0900

    add new files

---
+++ create_table.bat
@@ -0,0 +1,9 @@
+﻿
+php artisan make:model Models/Tweet -m
+
+php artisan make:model Models/Comment -m
+
+php artisan make:model Models/Favorite -m
+
+php artisan make:model Models/Follower -m
+
---
+++ seeder_create.bat
@@ -0,0 +1,5 @@
+php artisan make:seeder UsersTableSeeder
+php artisan make:seeder TweetsTableSeeder
+php artisan make:seeder CommentsTableSeeder
+php artisan make:seeder FavoritesTableSeeder
+php artisan make:seeder FollowersTableSeeder
